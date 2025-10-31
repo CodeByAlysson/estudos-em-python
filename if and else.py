ef3456a270b81c662a2d773057bbc789758ef7af
Alysson <alyssonmelo.ti@gmail.com>
2025-10-31 13:41:12 -0300

    primeira alteração

---
+++ if and else.py
@@ -0,0 +1,8 @@
+#Neste exercício vou te explicar como usar if e else em Python.
+
+idade = int(input("Digite sua idade: "))
+
+if idade >=18:
+    print("Você é maior de idade e já pode dirigir.")
+else:
+    print("Você é menor de idade. Aguarde completar 18 anos para tirar sua CNH.")
