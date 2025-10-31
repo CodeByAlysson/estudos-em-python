d365fc68b7c1e6e67394481104c21481d812dcc0
Alysson <alyssonmelo.ti@gmail.com>
2025-10-31 13:46:58 -0300

    terceira alteração

--- if and else.py
+++ if and else.py
@@ -5,4 +5,4 @@ idade = int(input("Digite sua idade: "))
 if idade >=18:
     print("Você é maior de idade e já pode dirigir.")
 else:
-    print("Você é menor de idade. Aguarde completar 18 anos!")
+    print("Você é menor de idade. Aguarde completar 18 anos!") 
