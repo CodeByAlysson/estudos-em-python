551ca05009cad3254b8d5065d7a40e85a650e848
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-02 14:58:09 -0300

    priemira alteração

--- while true.py
+++ while true.py
@@ -1,9 +1,10 @@
 # Neste exercício vou te explicar como usar o loop while em Python.
 
 while True:
-    nome = input('Digite o nome correto para acessar o sistema: ')
-    if nome == "Alysson":
-        print("Acesso permitido. Bem-vindo, Alysson!")
+    nome = input("Digite o nome correto para acessar o sistema: ")
+
+    if nome == "CodeByAlysson":
+        print("Acesso permitido. Bem-vindo, CodeByAlysson ! ✅")
         break
     else:
-        print("Nome incorreto. Tente novamente ou caia fora!!.")
+        print("Nome incorreto. Tente novamente ou caia fora ! ❌.")
