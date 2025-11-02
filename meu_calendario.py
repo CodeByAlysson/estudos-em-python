98faaebc94d61281810e931c9068467eb9865883
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-02 16:17:48 -0300

    Meu calendário em Python

---
+++ meu_calendario.py
@@ -0,0 +1,9 @@
+# Nesta seção de estudo te ensino a como criar um calendário em Python usando o módulo calendar.
+
+import calendar
+yy = 2025
+mm = 11
+
+# display do calendário
+
+print(calendar.month(yy, mm))
